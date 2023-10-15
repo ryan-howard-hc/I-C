1d254c7d6da7eddf943e5a49a1b6ec8913649fd6
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-10-15 18:02:51 -0400

    learning how to use api keys and .env files in blazor

--- MyBlazorApp/Program.cs
+++ MyBlazorApp/Program.cs
@@ -1,11 +1,26 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MyBlazorApp;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using DotNetEnv;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace MyBlazorApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            // Load environment variables from the .env file
+            DotNetEnv.Env.Load();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
-await builder.Build().RunAsync();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
